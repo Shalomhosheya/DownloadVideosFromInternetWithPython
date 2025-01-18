69a862f51708c951bfb33120061b7b83df2a17b1
KD ShalomHosheya Jayathilaka <129625075+Shalomhosheya@users.noreply.github.com>
2025-01-18 22:56:37 +0530

    initial commit

--- Run2.py
+++ Run2.py
@@ -1,7 +1,25 @@
 import yt_dlp
 
-link = input("Enter YouTube link: ")
-ydl_opts = {'outtmpl': '%(title)s.%(ext)s'}
+def list_formats(link):
+    with yt_dlp.YoutubeDL() as ydl:
+        info = ydl.extract_info(link, download=False)
+        print("\nAvailable formats:")
+        for fmt in info['formats']:
+            print(f"Format ID: {fmt['format_id']} | Resolution: {fmt.get('resolution', 'N/A')} | "
+                  f"Extension: {fmt['ext']} | Video Codec: {fmt.get('vcodec', 'N/A')}")
+
+link = input("Enter the link: ")
+
+list_formats(link)
+
+format_id = input("\nEnter the Format ID of the desired resolution: ")
+
+ydl_opts = {
+    'format': format_id, 
+    'outtmpl': '%(title)s.%(ext)s',  
+}
+
 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
     ydl.download([link])
+
 print("Video downloaded successfully!")
